6c4c6a8290d486d16befeee5406e14710ea799a0
erykzarebski <erykzarebski0@gmail.com>
2023-01-24 21:19:44 +0100

    Change port

--- server/config/cfg.ts
+++ server/config/cfg.ts
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 import { Secret } from "jsonwebtoken";
 import { resolve } from "path";
 
+const path = resolve(__dirname, "../../.dev.env");
+dotenv.config({ path });
 
-const path = resolve(__dirname, "../../.dev.env")
-dotenv.config({path});
-
-export const { MONGODB_URI, PORT } = process.env;
+export const { PORT } = process.env || 30498;
+export const { MONGODB_URI } = process.env;
 export const SECRET = process.env.SECRET as Secret;

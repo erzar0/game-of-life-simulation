b6c72d662cd0f11723bbb725f09932e64a8cf89d
erykzarebski <erykzarebski0@gmail.com>
2023-01-24 21:47:35 +0100

    Branch for render.com

--- client/src/services/gridConfig.ts
+++ client/src/services/gridConfig.ts
@@ -1,6 +1,7 @@
 import { GridConfig } from "../types/Grid";
 import getBearer from "../utils/getBearer";
-const baseUrl = "http://localhost:30498/grid-config";
+
+const baseUrl = "https://game-of-life-api.onrender.com/grid-config";
 
 const getUserConfigs = async () => {
   const bearer = getBearer();
